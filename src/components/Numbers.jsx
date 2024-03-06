4ebab8fb9d83904eace4e451b9326bac6ff29b1b
Ezz dghedy <ezzmohsend@gmail.com>
2024-03-06 12:21:57 +0200

    adj

--- src/components/Numbers.jsx
+++ src/components/Numbers.jsx
@@ -6,7 +6,7 @@ const Numbers = () => {
       <h1 className="underline capitalize text-xl md:text-2xl lg:text-3xl font-bold tracking-wider mt-8 mb-16">
         ace tech in numbers
       </h1>
-      <div className=" w-full flex flex-col justify-center  md:justify-evenly px-5 text-primary text-lg md:text-xl lg:text-2xl items-center md:flex-row md:flex-wrap ">
+      <div className=" w-full flex flex-col justify-center  md:justify-evenly px-5 text-primary text-lg md:text-xl lg:text-2xl items-center md:flex-row md:flex-wrap lg:grid lg:grid-cols-4 ">
         {numbers.map((number) => {
           return (
             <div
